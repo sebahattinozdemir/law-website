f45f78ca9ef19fa396819d13b5c2f88146c1c5d3
Berati Şahin <beratis@atptech.net>
2020-11-01 20:59:22 +0300

    content organized

--- src/components/services-page/Service.js
+++ src/components/services-page/Service.js
@@ -12,81 +12,64 @@ function Service() {
                 title="Hizmetlerimiz"
                 icon={<GavelIcon style={{fontSize:"35px"}}/>}
             />
-
-            <div class="col-12 py-4 px-0" style={{ width:"100%",margin:"auto"}}>
+            
+            <div className="inter" style={{backgroundColor:"#F7ECEC",witdth:"100%",overflow:"auto",paddingTop:"5px"}}>
+                   
+                    
           
-                    <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-                        <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
+                            
+                            
+                                <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-           
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-                        <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
+                            
+                            
+                                <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-            
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-                        <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
+                            
+                            
+                                <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-            
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-                        <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
+                            
+                            
+                                <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-            
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-                        <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
+                            
+                            
+                                <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-           
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-                        <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
+                            
+                            
+                                <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-            
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-                        <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
+                            
+                            
+                                <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-            
-                    </div>
-
-                    <div className="col-lg-3 col-md-6 col-sm-6 col-12">
-                        <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
-                        <div class="card-body">
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="col-lg-3 col-md-6 col-sm-6 col-12">
+                            
+                            
+                                <img src={h1} class="card-img-top" alt="..." style={{width:"100%", height:"auto"}}></img>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         </div>
-            
-                    </div>
-
-         
-
-         
-            
+                        
+                  
+ 
+            </div>
         </div>
-        
-        </div>
-
     )
 }
 
