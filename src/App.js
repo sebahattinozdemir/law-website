b180e021e33c7d8d21bcb11b12f9b2958010cbc9
sebahattin <sebahattin@veriyaz.com.tr>
2020-12-17 11:52:23 +0300

    exact

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ function App() {
               />
             ))}
 
-            <Route path="/admin" component={Admin} />
+            <Route exact path="/admin" component={Admin} />
             <Route component={Notfound} />
           </Switch>
           <div
